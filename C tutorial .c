a9e8d321539ade5ce74f2b47b00d41c044914073
nurooj691 <nurooj691@gmail.com>
2024-08-31 11:21:58 +0500

    Update C tutorial .c

--- C tutorial .c
+++ C tutorial .c
@@ -1,11 +1,6 @@
-#include<stdio.h>
-int main() {
-	char gender;
-	printf("please enter your gender?(M/F)");
-	scanf("%c", &gender);
-	gender == 'M' ? printf("Male") : printf("Female");
-	
-	return 0;
-	
-}
+The word Algorithm means ” A set of finite rules or instructions to be followed in calculations or other problem-solving
+	operations ”.
+Pseudocode is a detailed yet readable description of what a computer program or algorithm should do.
+GitHub is a web-based interface that uses Git, the open source version control software that lets multiple people make separate
+changes to web pages at the same time
 	
